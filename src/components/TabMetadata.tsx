fad3d8a13acb144e27f28f7f908b71da0de4507a
astra137 <maccelerated@gmail.com>
2024-07-16 20:29:18 -0700

    add link to fido-mds-explorer

--- src/components/TabMetadata.tsx
+++ src/components/TabMetadata.tsx
@@ -1,5 +1,6 @@
 import type { PublicKeyCredentialWithAttestationJSON } from '@github/webauthn-json'
 import { useMemo } from 'react'
+import { UuidTool } from 'uuid-tool'
 import { css } from '../../styled-system/css'
 import { container } from '../../styled-system/patterns'
 import { findEntry } from '../lib/mds/blob'
@@ -21,12 +22,17 @@ export function TabMetadata({ credential }: Props) {
 		[attestationObject.authData],
 	)
 
-	const mdsEntry = useMemo(() => {
-		if (authData.attestedCredentialData) {
-			return findEntry(authData.attestedCredentialData.aaguid)
-		}
-		return null
+	const aaguid = useMemo(() => {
+		return authData.attestedCredentialData
+			? UuidTool.toString([...authData.attestedCredentialData.aaguid])
+			: null
 	}, [authData])
+
+	const mdsEntry = useMemo(() => {
+		return aaguid ? findEntry(aaguid) : null
+	}, [aaguid])
+
+	const mdsExplorerUrl = `https://opotonniee.github.io/fido-mds-explorer/?aaguid=${aaguid}`
 
 	return (
 		<>
@@ -42,6 +48,17 @@ export function TabMetadata({ credential }: Props) {
 					maxW: '3xl',
 				})}
 			>
+				<section
+					className={css({ display: 'flex', flexDir: 'column', gap: 'md' })}
+				>
+					<a
+						className={css({ _hover: { color: 'green.500' } })}
+						href={mdsExplorerUrl}
+					>
+						{mdsExplorerUrl}
+					</a>
+				</section>
+
 				<section>
 					<h2>https://fidoalliance.org/metadata/</h2>
 					<JsonBlock data={mdsEntry} />
--- src/lib/mds/blob.ts
+++ src/lib/mds/blob.ts
@@ -1,7 +1,5 @@
-import { UuidTool } from 'uuid-tool'
 import blob from './blob.json'
 
-export function findEntry(aaguid: Uint8Array) {
-	const uuid = UuidTool.toString([...aaguid])
-	return blob.entries.find((x) => x.aaguid === uuid)
+export function findEntry(aaguid: string) {
+	return blob.entries.find((x) => x.aaguid === aaguid)
 }
